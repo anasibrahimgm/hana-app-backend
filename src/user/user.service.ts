2ab3aaabead947dad058f911e506b7fc2b083e20
anasibrahimgm <anasibrahimgm@gmail.com>
2022-12-17 18:40:54 +0100

    validate existing user & email format

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+} from '@nestjs/common';
+
 import { User } from './user.interface';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+
+var validator = require('validator');
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async register(user: User): Promise<User> {
+    const email = user.email;
+
+    const existingUser = await this.userModel.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestException({
+        email: 'User with this email already exists.',
+      });
+    }
+
+    if (validator.isEmail(email) === false) {
+      throw new BadRequestException({
+        email: 'invalid Email.',
+      });
+    }
+
     const newUser = new this.userModel(user);
     return await newUser.save();
   }
